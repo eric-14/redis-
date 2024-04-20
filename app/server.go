14f38f01558315941511d6c049be23aff6c6c8a6
eric-14 <ericmwas01@gmail.com>
2024-04-20 12:58:52 +0300

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -1,87 +1,65 @@
 package main
 
-
 import (
 	"fmt"
-	"net"
 	"log"
+	"net"
 	"os"
-
 )
 
 const (
 	HOST1 = "localhost"
-	PORT1 = "8080"
-	TYPE1 = "TCP"
+	PORT1 = "8000"
+	TYPE1 = "tcp"
 )
 
-/*
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	//
-	 l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	_, err = l.Accept()
-	if err != nil {
- 	fmt.Println("Error accepting connection: ", err.Error())
-	os.Exit(1)
-	 }
-}
-*/
-
-func main(){
 	listener, err := net.Listen(TYPE1, HOST1+":"+PORT1)
 
 	if err != nil {
+
+		fmt.Println("Failed to open a TCP port", err)
 		log.Fatal(err)
+
 		os.Exit(1)
 	}
-
 	defer listener.Close()
-
 	for {
+
 		conn, error := listener.Accept()
 
 		if error != nil {
 			log.Fatal("Failed to accept new clients in the TCP server \n")
-			return 
+			return
 		}
 
 		handleConn1(conn)
 		defer conn.Close()
+	}
+
 }
-}
 
-
-
-func handleConn1(conn net.Conn){
+func handleConn1(conn net.Conn) {
 	defer conn.Close()
 
+	fmt.Println("Hanle connection function")
 	inputData := make([]byte, 1024)
 
 	n, error := conn.Read(inputData)
 
 	if error != nil {
 		fmt.Println("Error reading bytes")
-		return 
+		return
 	}
 
 	// iterate through the message to get info PING
-	message:= "PONG\r\n"
+	message := "PONG\r\n"
 	responseMessage := []byte(message)
-	
-	for i:=0; i < n; i++ {
-		if inputData[i] == 'P' && inputData[i+1] =='i' && inputData[i+2] =='n' && inputData[i+3] == 'g' { //PING MESSAGE
+
+	for i := 0; i < n; i++ {
+		if inputData[i] == 'P' && inputData[i+1] == 'i' && inputData[i+2] == 'n' && inputData[i+3] == 'g' { //PING MESSAGE
 			conn.Write(responseMessage)
 		}
 	}
 
 }
-
-
