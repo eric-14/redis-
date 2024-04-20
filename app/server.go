3f2d0aa70a265c04dc7dd048f0cdbc0dad3752cb
eric-14 <ericmwas01@gmail.com>
2024-04-20 00:24:17 -0700

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -1,12 +1,21 @@
 package main
 
+
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
-	 "net"
-     "os"
+	"net"
+	"log"
+	"os"
+
 )
 
+const (
+	HOST = "localhost"
+	PORT = "8080"
+	TYPE = "TCP"
+)
+
+/*
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -24,3 +33,55 @@ func main() {
 	os.Exit(1)
 	 }
 }
+*/
+
+func main(){
+	listener, err := net.Listen(TYPE, HOST+":"+PORT)
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	defer listener.Close()
+
+	for {
+		conn, error := listener.Accept()
+
+		if error != nil {
+			log.Fatal("Failed to accept new clients in the TCP server \n")
+			return 
+		}
+
+		handleConn(conn)
+		defer conn.Close()
+}
+}
+
+
+
+func handleConn(conn net.Conn){
+	defer conn.Close()
+
+	inputData := make([]byte, 1024)
+
+	n, error := conn.Read(inputData)
+
+	if error != nil {
+		fmt.Println("Error reading bytes")
+		return 
+	}
+
+	// iterate through the message to get info PING
+	message:= "PONG\r\n"
+	responseMessage := []byte(message)
+	
+	for i:=0; i < n; i++ {
+		if inputData[i] == 'P' && inputData[i+1] =='i' && inputData[i+2] =='n' && inputData[i+3] == 'g' { //PING MESSAGE
+			conn.Write(responseMessage)
+		}
+	}
+
+}
+
+
