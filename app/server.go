0c26274af470061c8a99e714374f90fb3e204d1c
eric-14 <ericmwas01@gmail.com>
2024-04-21 14:45:18 +0300

    echo commands

--- app/server.go
+++ app/server.go
@@ -57,8 +57,8 @@ func handleConn1(conn net.Conn) {
 		}
 
 		// iterate through the message to get info PING
-		//message := "+PONG\r\n"
-		// responseMessage := []byte(message)
+		message := "+PONG\r\n"
+		responseMessage := []byte(message)
 		// numberOfPings := 1
 
 		if n == 0 {
@@ -71,15 +71,15 @@ func handleConn1(conn net.Conn) {
 		conn.Write([]byte(writeResponse))
 		// fmt.Println(string(inputData))
 
-		// for i := 0; i < n; i++ {
+		for i := 0; i < n; i++ {
 
-		// 	if inputData[i] == 'p' && inputData[i+1] == 'i' && inputData[i+2] == 'n' && inputData[i+3] == 'g' { //PING MESSAGE
+			if inputData[i] == 'p' && inputData[i+1] == 'i' && inputData[i+2] == 'n' && inputData[i+3] == 'g' { //PING MESSAGE
 
-		// 		conn.Write(responseMessage)
-		// 		fmt.Println("Responding with pong ", i, n, numberOfPings)
+				conn.Write(responseMessage)
+				fmt.Println("Responding with pong ", i, n)
 
-		// 	}
-		// }
+			}
+		}
 		//conn.Write([]byte("+PONG\r\n"))
 
 	}
