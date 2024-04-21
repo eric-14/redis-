f3a8dc02fc2e0285922564b1d1eef98003766af4
eric-14 <ericmwas01@gmail.com>
2024-04-21 17:22:16 +0300

    parsing echo

--- app/server.go
+++ app/server.go
@@ -77,7 +77,7 @@ func handleConn1(conn net.Conn) {
 
 			if inputData[i] == 'p' && inputData[i+1] == 'i' && inputData[i+2] == 'n' && inputData[i+3] == 'g' { //PING MESSAGE
 
-				conn.Write([]byte("pong\r\n"))
+				conn.Write([]byte("PONG\r\n"))
 				fmt.Println("Responding with pong ", i, n)
 
 			}
