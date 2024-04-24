87e026118be26d08603ac9c9fdc0f6cdc240a191
eric-14 <ericmwas01@gmail.com>
2024-04-24 21:44:29 +0300

    adding expiry date

--- app/server.go
+++ app/server.go
@@ -235,7 +235,7 @@ func executingFunction(fn int, key string, value string) (string, error){
 						time.Time{},
 					}
 
-		if *(timetracker1[key]) != dataEmpty {
+		if *(timetracker1[key]).value != "" {
 			fmt.Println("line 241 time time tracker in fn get ", timetracker1[key])
 			// if the data type has a time tracker then execute time function 
 			timedata := timetracker1[key]  // time information about 
