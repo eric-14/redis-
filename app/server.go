a15a32bf23055ca3fe4783f9cca556849199107b
eric-14 <ericmwas01@gmail.com>
2024-04-24 13:25:53 +0300

    adding expiry date

--- app/server.go
+++ app/server.go
@@ -24,7 +24,7 @@ type Data1 struct {
 
 }
 
-var timetracker1 map[string]Data1
+var timetracker1 map[string]*Data1
 
 
 
@@ -272,5 +272,5 @@ func keyValue(input []byte) ([]string, error) {
 func timetracker(fn int, key string, value string, data *Data1){
 	string1, _ := executingFunction(0, key, value)
 	fmt.Println(string1)
-	timetracker1[key] = *data
+	timetracker1[key] = data
 }
