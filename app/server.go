e805411fa7d7cd5e79f5942a8c09864f91cb2847
eric-14 <ericmwas01@gmail.com>
2024-04-24 13:29:53 +0300

    adding expiry date

--- app/server.go
+++ app/server.go
@@ -270,6 +270,7 @@ func keyValue(input []byte) ([]string, error) {
 }
 
 func timetracker(fn int, key string, value string, data Data1){
+	fmt.Println("line 273")
 	string1, _ := executingFunction(0, key, value)
 	fmt.Println(string1)
 	timetracker1[key] = &data
